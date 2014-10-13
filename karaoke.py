fe7e3173b2cff36bff98fcd81aadd26247edb03c
abel vargas vera <avera@iota04.aulas.gsyc.es>
2014-10-13 10:22:18 +0200

    ejercicio 5 terminado

--- karaoke.py
+++ karaoke.py
@@ -13,21 +13,21 @@ parser = make_parser()
 Handler = SmallSMILHandler()
 parser.setContentHandler(Handler)
 try:
-	parser.parse(open(listarg[1]))
+    parser.parse(open(listarg[1]))
 except:
-	sys.exit('Usage: python karaoke.py file.smil')  # y despues se para la ej.
+    sys.exit('Usage: python karaoke.py file.smil')  # y despues se para la ej.
 List = Handler.get_tags()
 #ListOriginal = Handler.get_tags()
 for Dicc in List:
-	print Dicc['etiqueta'], '\t',  # Con coma al final no cambia de linea
-	for Atrib in Dicc:
-		if Atrib != 'etiqueta' and Dicc[Atrib] != "":
-			if Atrib == 'src':
-				recurso = Dicc[Atrib]
-				os.system("wget -q " + recurso)
-				NombreLocal = recurso.split('/')[-1]  # Me quedo con lo último
-				Dicc[Atrib] = NombreLocal  # Que es el nombre de lo descargado
-			print  Atrib, '= "' + Dicc[Atrib] + '" \t',
-			
-	print  # escribe una linea en blanco
+    print Dicc['etiqueta'], '\t',  # Con coma al final no cambia de linea
+    for Atrib in Dicc:
+        if Atrib != 'etiqueta' and Dicc[Atrib] != "":
+            if Atrib == 'src':
+                recurso = Dicc[Atrib]
+                os.system("wget -q " + recurso)
+                NombreLocal = recurso.split('/')[-1]  # Me quedo con lo último
+                Dicc[Atrib] = NombreLocal  # Que es el nombre de lo descargado
+            print  Atrib, '= "' + Dicc[Atrib] + '" \t',
+            
+    print  # escribe una linea en blanco
 
